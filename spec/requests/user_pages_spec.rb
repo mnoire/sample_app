88b1f81fc884a47e7a8ece7437da045971679b6e
Nicole Otero <nicoleotero11@gmail.com>
2012-04-16 16:16:55 -0400

    Debug specs

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,18 +10,20 @@ describe "User pages" do
     before { visit user_path(user) }
 
     it { should have_selector('h1',    text: user.name) }
-    it { should have_selector('title', text: user.name) }
+        it { should have_selector('title', text: user.name) }
   end
+  
   describe "signup" do
-
       before { visit signup_path }
 
-      describe "with invalid information" do
-        it "should not create a user" do
-          expect { click_button "Create my account" }.not_to change(User, :count)
+      let(:submit) { "Create my account" }
+
+          describe "with invalid information" do
+            it "should not create a user" do
+              expect { click_button submit }.not_to change(User, :count)
         end
       end
-
+      
       describe "with valid information" do
         before do
           fill_in "Name",         with: "Example User"
@@ -35,6 +37,14 @@ describe "User pages" do
             click_button "Create my account"
           end.to change(User, :count).by(1)
       end
-    end
+      
+      # describe "after saving the user" do
+      #     before { click_button submit }
+      #     let(:user) { User.find_by_email('user@example.com') }
+      #     
+      #     it { should have_selector('title', text: user.name) }
+      #     it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      # end
   end
+end
 end
