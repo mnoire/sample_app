488d50efbfcab460532053fea3c3811121c295c5
Nicole Otero <nicoleotero11@gmail.com>
2012-04-27 23:55:14 -0400

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,6 +25,7 @@ require 'spec_helper'
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   
   it { should be_valid }
@@ -104,6 +105,10 @@ require 'spec_helper'
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
     end
+  
+  describe "remmeber token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end  
 end
-
+end
